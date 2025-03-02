9e4f5888f19cb249e3333f1c922f58f1e8fc4b8b
DefenseArt <82384826+DefenseArt@users.noreply.github.com>
2025-03-02 19:37:31 +0900

    Create game.js

---
+++ code/keydown/game.js
@@ -0,0 +1,40 @@
+const start = document.getElementById("start");
+const target = document.getElementById("target");
+const score = document.getElementById("score");
+
+let randomUppercase = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+let scor = 0;
+
+let timer; 
+
+function startTimer() {
+    clearTimeout(timer); 
+    timer = setTimeout(() => {
+        console.log("시간 초과!"); 
+    }, 3000); 
+}
+
+
+start.addEventListener("click", () => {
+    randomUppercase = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    target.innerText = randomUppercase; 
+    startTimer();
+});
+
+document.addEventListener("keydown", (event) => {
+    console.log(`현재 표시된 글자: ${target.innerText}, 입력된 키: ${event.key}`);
+
+    if (!target.innerText) return;  
+
+    if(event.key === target.innerText) {
+        clearTimeout(timer);
+        
+        scor++;
+        score.innerText = `점수 : ${scor}`;
+
+        randomUppercase = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+        target.innerText = randomUppercase; 
+
+        startTimer();
+    }
+});
