6802aa8f18ebef068c60147e3a511505424b9d56
DefenseArt <82384826+DefenseArt@users.noreply.github.com>
2025-03-04 17:12:12 +0900

    Update Counter.js

--- code/Counter/Counter.js
+++ code/Counter/Counter.js
@@ -1,6 +1,7 @@
 const number  = document.getElementById("number");
 const increase = document.getElementById("increase");
 const decrease = document.getElementById("decrease");
+const reset = document.getElementById("reset");
 
 increase.onclick = () => {
     const sum = parseInt(number.innerText, 10);
@@ -10,4 +11,8 @@ increase.onclick = () => {
 decrease.onclick = () => {
     const sum = parseInt(number.innerText, 10);
     number.innerText = sum - 1;
+};
+
+reset.onclick = () => {
+    number.innerText = 0;
 };
